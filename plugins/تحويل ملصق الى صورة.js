a8af38001b89203dcae0603610c75a23595bf024
Misho64 <61743234+Misho64@users.noreply.github.com>
2024-02-03 22:50:22 +0200

    Update and rename convertidor-toimg.js to تحويل ملصق الى صورة.js

--- plugins/تحويل ملصق الى صورة.js
+++ plugins/تحويل ملصق الى صورة.js
@@ -1,6 +1,6 @@
 import {webp2png} from '../lib/webp2mp4.js';
 const handler = async (m, {conn, usedPrefix, command}) => {
-  const notStickerMessage = `*[❗𝐈𝐍𝐅𝐎❗] 𝚁𝙴𝚂𝙿𝙾𝙽𝙳𝙰 𝙰𝙻 𝚂𝚃𝙸𝙲𝙺𝙴𝚁 𝚀𝚄𝙴 𝙳𝙴𝚂𝙴𝙴 𝙲𝙾𝙽𝚅𝙴𝚁𝚃𝙸𝚁 𝙴𝙽 𝙸𝙼𝙰𝙶𝙴𝙽 𝙲𝙾𝙽 𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 ${usedPrefix + command}*`;
+  const notStickerMessage = `*قم بالرد على الملصق الذي تريد تحويله لصوره ${usedPrefix + command}*`;
   if (!m.quoted) throw notStickerMessage;
   const q = m.quoted || m;
   const mime = q.mediaType || '';
@@ -11,5 +11,5 @@ const handler = async (m, {conn, usedPrefix, command}) => {
 };
 handler.help = ['toimg (reply)'];
 handler.tags = ['sticker'];
-handler.command = ['toimg', 'jpg', 'img'];
+handler.command = ['صوره', 'صورة']
 export default handler;
