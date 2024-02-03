763a86cdcb895046402d78a1166876eea33d6553
Misho64 <61743234+Misho64@users.noreply.github.com>
2024-02-03 22:39:31 +0200

    Update and rename sticker-emojimix.js to استيكر دمج.js

--- plugins/استيكر دمج.js
+++ plugins/استيكر دمج.js
@@ -3,7 +3,7 @@ import MessageType from '@whiskeysockets/baileys';
 import fetch from 'node-fetch';
 import fs from 'fs';
 const handler = async (m, {conn, text, args}) => {
-  if (!args[0]) throw '*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝙻 𝚄𝚂𝙾 𝙳𝙴 𝙴𝚂𝚃𝙴 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 𝙳𝙴𝙱𝙴 𝚂𝙴𝚁 #emojimix <emoji 1>&<emoji 2>*\n*𝙴𝙹𝙴𝙼𝙿𝙻𝙾:*\n*#emojimix 🤨&😣*';
+  if (!args[0]) throw '*باستخدام هذا الأمريجب ان يكون #دمج <ايموجي 1>+<ايموجي 2>*\n*مثال:*\n*.دمج 🤨+😣*';
   const [emoji1, emoji2] = text.split`&`;
   const anu = await fetchJson(`https://tenor.googleapis.com/v2/featured?key=AIzaSyAyimkuYQYF_FXVALexPuGQctUWRURdCYQ&contentfilter=high&media_filter=png_transparent&component=proactive&collection=emoji_kitchen_v5&q=${encodeURIComponent(emoji1)}_${encodeURIComponent(emoji2)}`);
   for (const res of anu.results) {
@@ -11,9 +11,9 @@ const handler = async (m, {conn, text, args}) => {
     conn.sendFile(m.chat, stiker, null, {asSticker: true});
   }
 };
-handler.help = ['emojimix'].map((v) => v + ' emot1|emot2>');
+handler.help = ['دمج'].map((v) => v + ' emot1|emot2>');
 handler.tags = ['fun'];
-handler.command = /^(emojimix)$/i;
+handler.command = /^(دمج)$/i
 export default handler;
 const fetchJson = (url, options) => new Promise(async (resolve, reject) => {
   fetch(url, options)
